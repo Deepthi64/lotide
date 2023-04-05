1fb9e8f3e79366f04bc3e0199f8d0ec73a084e2d
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-05 01:45:22 -0400

    used template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,11 @@
 // FUNCTION IMPLEMENTATION
+const failed ="Lighthouse Labs !== Bootcamp";
+const passed = "1 === 1";
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    console.log("Error 🛑🛑🛑: assertion failed");
+    console.log(`Error 🛑🛑🛑:assertion failed ${failed}`);
   } else {
-    console.log("Error ✔✔✔: assertion passed");
+    console.log(`Error ✔✔✔: assertion passed  ${passed}`);
   }
 };
 
