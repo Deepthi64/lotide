718100e802a86cfcde5b7a426c18e5d37406673a
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-05 22:58:34 -0400

    initial commit

---
+++ flatten.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const flatten = function(array) {
+  let flattenedArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      flattenedArray = flattenedArray.concat(array[i]);
+    } else {
+      flattenedArray.push(array[i]);
+    }
+  }
+  return flattenedArray;
+};
+const input = [1, 2, [3, 4], 5, [6]];
+const expectedOutput = [1, 2, 3, 4, 5, 6];
+assertArraysEqual(flatten(input), expectedOutput);
