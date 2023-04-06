e2e5259c99f76801351e03ddb92afcec27d717cc
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-06 02:26:04 -0400

    intial commit

---
+++ eqObjects.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+
+  for (const key of object1Keys) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // expected output: true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // expected output: true
