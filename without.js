164f5a68518de1b25c477ca04a206d4310581df4
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-05 22:33:02 -0400

    intial commit

---
+++ without.js
@@ -0,0 +1,39 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i]);
+    }
+  }
+  return newArray;
+};
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+assertArraysEqual(without([], []), []);
+assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]);
+assertArraysEqual(without(["hello", "world", "lighthouse"], ["lighthouse"]), ["hello", "world"]);
+assertArraysEqual(without(["hello", "world", "lighthouse"], ["lighthouse", "hello"]), ["world"]);
+assertArraysEqual(without(["hello", "world", "lighthouse"], ["goodbye"]), ["hello", "world", "lighthouse"]);
+
+// Ensure that the original array was not modified
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
