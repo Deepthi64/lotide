61750728ed987eb1032855bc289f6eee1f92c084
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-09 23:48:00 -0400

    intial commit

---
+++ assertObjectsEqual.js
@@ -0,0 +1,29 @@
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+
+  if (object1Keys.length !== object2Keys.length) {
+    return false;
+  }
+
+  for (const key of object1Keys) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+// Test cases
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }); 
+assertObjectsEqual({ a: '1', b: 2 }, { a: '1', b: 2, c: '3' });
