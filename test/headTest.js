e9104b68c775dd260c162c46219cc2cd3d20a626
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-11 02:43:53 -0400

    updated using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,12 @@
-const assertArraysEqual = require('../assertArraysEqual');
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // Output: ✅✅✅ Assertion Passed: 1,2,3 === 1,2,3
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // Output: 🛑🛑🛑 Assertion Failed: 1,2,3 !== 3,2,1
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Output: ✅✅✅ Assertion Passed: 1,2,3 === 1,2,3
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Output: 🛑🛑🛑 Assertion Failed: 1,2,3 !== 1,2,3
+
+const assert = require('chai').assert;
+const head = require('../head').head;
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
