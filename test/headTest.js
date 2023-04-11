5b4ea2814ef886c4bdfaab61d52ed2c7f3275812
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-11 01:03:49 -0400

    example for module exports

---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual');
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // Output: ✅✅✅ Assertion Passed: 1,2,3 === 1,2,3
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // Output: 🛑🛑🛑 Assertion Failed: 1,2,3 !== 3,2,1
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // Output: ✅✅✅ Assertion Passed: 1,2,3 === 1,2,3
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // Output: 🛑🛑🛑 Assertion Failed: 1,2,3 !== 1,2,3
