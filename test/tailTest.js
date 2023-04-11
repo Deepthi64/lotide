df548501f5482df55c17ddece6876fc3b7ed3c18
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-11 03:23:22 -0400

    updated using mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,18 @@
 
 //const tail = require('../tail');
 //const assertArraysEqual = require('../assertArraysEqual');
-const { tail, assertArraysEqual } = require('../tail');
+//const { tail, assertArraysEqual } = require('../tail');
 
-const expected = ["Lighthouse", "Labs"];
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(result, expected);
+//const expected = ["Lighthouse", "Labs"];
+//const result = tail(["Hello", "Lighthouse", "Labs"]);
+//assertArraysEqual(result, expected);
+const assert = require('chai').assert;
+const tail = require('../tail').tail;
+
+describe('#tail', () => {
+  it('returns ["Lighthouse", "Labs"] when called with ["Hello", "Lighthouse", "Labs"]', () => {
+    const expected = ["Lighthouse", "Labs"];
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.deepEqual(result, expected);
+  });
+});
