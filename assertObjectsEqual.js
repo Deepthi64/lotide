d24f84b8bf954f8e16297e5024a75074d8d05c54
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-05-21 23:12:25 -0700

    fix : add test and refactoring code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,19 +1,4 @@
-const eqObjects = function(object1, object2) {
-  const object1Keys = Object.keys(object1);
-  const object2Keys = Object.keys(object2);
-
-  if (object1Keys.length !== object2Keys.length) {
-    return false;
-  }
-
-  for (const key of object1Keys) {
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const { eqObjects } = require('./eqObjects');
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -24,6 +9,4 @@ const assertObjectsEqual = function(actual, expected) {
   }
 };
 
-// Test cases
-assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' }); 
-assertObjectsEqual({ a: '1', b: 2 }, { a: '1', b: 2, c: '3' });
+module.exports = {assertObjectsEqual};
