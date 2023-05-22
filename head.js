043e86221c4a229b7d74c1a426d433f7958fc438
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-05-22 05:05:34 -0700

    fix : clean code

--- head.js
+++ head.js
@@ -6,10 +6,7 @@ const head = function(array) {
   }
   return firstItem;
 };
-//assertEqual(head([3]), 3);
-//assertEqual(head([]), undefined);
-//assertEqual(head([5,6,7]), 5);
-//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
 
 module.exports = {head};
 
